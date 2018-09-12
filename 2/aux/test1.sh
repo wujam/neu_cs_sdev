13c53c82ac9ac8d98dcf4372f3a278e97cb9198b
Sreeharsha Potu <potu.s@husky.neu.edu>
2018-09-11 21:50:03 -0400

    wrote and testing 2

--- 2/aux/test1.sh
+++ 2/aux/test1.sh
@@ -4,12 +4,11 @@
 # ./test.sh path/to/2 test_number
 # or
 # ./test.sh path/to/2
-if [ $2 -eq 0]
+if [[ $2 -eq 0 ]];
 then
-	COUNTER=ls -1 ./sample-inputs | wc -l
-	for i in COUNTER;
+	for filename in ./sample-inputs/*.txt;
 	do
-		$1 < sample-inputs/$i.txt | diff - expected-output/$i.txt
+		$1 < sample-inputs/${filename##*/} | diff - expected-output/${filename##*/}
 	done
 else
 	$1 < sample-inputs/$2.txt | diff - expected-output/$2.txt
