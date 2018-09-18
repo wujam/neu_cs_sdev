cd6a82c58b40572731606844b3af058d6d35625d
James Wu <wujam@github.com>
2018-09-18 02:51:36 -0400

    fix typos and module format

---
+++ 3/spread/__init__.py
@@ -0,0 +1,4 @@
+# __all__ = ["datatypes", "spread_sheet_op", "basic_spread_sheet"]
+from .datatypes import *
+from .spread_sheet_op import *
+from .basic_spread_sheet import *
--- 3/spread/basic_spread_sheet.py
+++ 3/spread/basic_spread_sheet.py
@@ -1,6 +1,5 @@
-import datatypes
-import spread_sheet_op
-
+from .datatypes import *
+from .spread_sheet_op import *
 class BasicSpreadSheet(SpreadSheetOp):
     """
     Spread sheet implementation.
@@ -10,30 +9,31 @@ class BasicSpreadSheet(SpreadSheetOp):
 
     def __init__(self, formulas):
         self.formulas = formulas
-        _sheet_inst = self 
+        self.__class__._sheet_inst = self 
 
     def valueAt(position):
-        formula = _sheet_inst.formulas[position[0]][position[1]]
+        formula = BasicSpreadSheet._sheet_inst.formulas[position[0]][position[1]]
 
         def eval_value(value):
             """
             Evaluates a value.
             value: a Value
             """
-            if (isinstance(value.value, (int, long, float, complex))):
+            if (isinstance(value.value, (int, float))):
                 return value.value
             else:
-                return _sheet_inst.valueAt(*value.value)
+                return BasicSpreadSheet.valueAt(value.value)
 
         val_total = eval_value(formula.value)
 
         while(formula.operation != None and formula.operation.op != Op.NONE):
             cur_op = formula.operation.op
-            cur_value = eval_value(formula.operation.formula.value)
+            cur_value = eval_value(formula.operation.formulas.value)
             if (cur_op == Op.ADD):
                 val_total = val_total + cur_value
             else:
                 val_total = val_total * cur_value
+            formula = formula.operation.formulas
 
         return val_total
     
