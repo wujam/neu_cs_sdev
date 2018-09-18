905a8edef06dbedb40cfd1e9747783546ca2e333
James Wu <wujam@github.com>
2018-09-18 01:46:33 -0400

    change NOOP to NONE and add documentation and more methods

--- 3/spread/datatypes.py
+++ 3/spread/datatypes.py
@@ -6,12 +6,25 @@ class Value:
     Is either a constant or a reference to a valid position of another cell
     """
     def __init__(self, value):
+        """
+        Constructs a Value object with the given value
+        value: Number or a (Integer, Integer) representing row, column
+        """
         self.value = value
 
-class Op:
-    NOOP = 0
+class Op(Enum):
+    NONE = 0
     ADD = 1
     MUL = 2
+
+    @classmethod
+    def is_op(cls, val):
+        """
+        Check if the given is in the enum.
+        cls: Op class
+        val: Value to check
+        """
+        return any(op.value == val for op in cls)
 
 class Operation:
     """
@@ -19,6 +32,11 @@ class Operation:
     Contains 2 fields, op and formulas
     """
     def __init__(self, operation, formulas):
+        """
+        Constructs a Operation object with an Op and a Formulas.
+        operation: Op, the op to use
+        formulas: Formulas, the formulas to use
+        """
         self.op = operation
         self.formulas = formulas
 
@@ -27,5 +45,10 @@ class Formulas:
     Class to represent formulas
     """
     def __init__(self, value, operation):
+        """
+        Constructs a Formulas object with a Value and an Operation
+        value: Value, the value to use
+        operation: Operation, the operation to use
+        """
         self.value = value
         self.operation = operation
