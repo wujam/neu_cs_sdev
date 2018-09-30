468bf38741e1259b0633ea1045b80ea7147579da
Sreeharsha Potu <potu.s@husky.neu.edu>
2018-09-30 17:00:17 -0400

    added test for jsonschema

---
+++ 6/aux/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+import json
+import jsonschema
+from jsonschema import validate
+
+instance = ['']
+
+schema = {'type': 'array', 'items': {'$ref': 'address.json'}}
+
+resolver = jsonschema.RefResolver("file://%s/jsonschemas/" % os.path.abspath(os.path.dirname(__file__)), schema)
+
+print("file://%s/jsonschemas/" % os.path.abspath(os.path.dirname(__file__)))
+
+print(validate(instance, schema, resolver=resolver))
