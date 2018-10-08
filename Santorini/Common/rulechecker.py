8fa79026641ebd6aa5cd7d83a846becc5525116a
Sreeharsha Potu <potu.s@husky.neu.edu>
2018-10-08 17:49:18 -0400

    added is_valid_move_and_build method

--- Santorini/Common/rulechecker.py
+++ Santorini/Common/rulechecker.py
@@ -25,13 +25,10 @@ class RuleChecker:
     @direction_to_move: tuple of an int within [-1, 1] and [-1, 1]. The first number specifies the x direction to move,
                         and the second number represents the y direction to move. Positive x is east, positive y is south.
                         cannot be (0, 0)
-    @direction_to_build: tuple of an int within [-1, 1] and [-1, 1]. The numbers specify the direction to build relative
-                         to the new position of the worker. If the move is game winning, this should be specified as (0,0).
-                         cannot be (0, 0)
     @return: True if the move is valid, False otherwise.
 
     """
-    def is_move_valid(self, player_number: int, worker_number: int, direction_to_move: (int, int), direction_to_build: (int, int)) -> bool:
+    def is_move_valid(self, player_number: int, worker_number: int, direction_to_move: (int, int)):
         if (not self._workers_are_placed()):
             return False
 
@@ -53,6 +50,30 @@ class RuleChecker:
         worker_positions = self._flatten(players)
         if (new_worker_position in worker_positions):
             return False
+
+        return True
+
+    """
+    determines if a move is valid based on the phase of the game, player turn, and the given move
+    @player_number: 1 or 2, the number of the player of the worker that's moving
+    @worker_number: 1 or 2, which worker of the player is moving
+    @direction_to_move: tuple of an int within [-1, 1] and [-1, 1]. The first number specifies the x direction to move,
+                        and the second number represents the y direction to move. Positive x is east, positive y is south.
+                        cannot be (0, 0)
+    @direction_to_build: tuple of an int within [-1, 1] and [-1, 1]. The numbers specify the direction to build relative
+                         to the new position of the worker. If the move is game winning, this should be specified as (0,0).
+                         cannot be (0, 0)
+    @return: True if the move is valid, False otherwise.
+
+    """
+    def is_move_and_build_valid(self, player_number: int, worker_number: int, direction_to_move: (int, int), direction_to_build: (int, int)) -> bool:
+        # check if the move is valid
+        if (not self.is_move_valid(player_number, worker_number, direction_to_move)):
+            return False
+
+        worker_position = self.board.get_worker_position(player_number, worker_number)
+        new_worker_position = self._add_pos_and_move(worker_position, direction_to_move)
+        players = self.board.get_worker_positions()
 
         # building bounds checking
         build_position = self._add_pos_and_move(new_worker_position, direction_to_build)
@@ -161,7 +182,7 @@ class RuleChecker:
     @worker: tuple of two ints (x, y) within the bounds of the board [0,6)
     @return: a list of positions that a worker can move to
     """
-    def _where_can_worker_build(self, worker)
+    def _where_can_worker_build(self, worker):
         possible_moves = self._where_can_worker_move(worker)
         buildable_pos = []
         for poss_move in possible_moves:
