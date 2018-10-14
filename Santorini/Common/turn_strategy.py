83170ac4c410170bc0c6ddd6a695b87176395937
James Wu <wujam@github.com>
2018-10-14 17:05:30 -0400

    made signatures less verbose and removed pointless helper method

--- Santorini/Common/turn_strategy.py
+++ Santorini/Common/turn_strategy.py
@@ -1,7 +1,31 @@
 import itertools
 from functools import reduce
+
 """
 A sub-strategy that determines the next move to make outside of the start up phase
+
+A BuildingGrid is a 2d list of ints from [0-4]. The outer list is a list
+    of columns, and the inner list is a list of heights representing
+    the height of each building. The positive direction of the outer
+    list represents "East" and the positive direction of the inner list
+    represents "South".
+
+A Player is a list of Workers, which are tuples of two ints
+    (x, y) representing their position on the board.
+    The x and y directions are in the same convention as the buildings'.
+    No Workers in a Player can be in the same position as one another.
+A PlayerList is a list of two Players.
+    No Workers among all Players can be in the same position as one
+    another.
+A Turn is a tuple of (worker_number: int, move_direction: Direction,
+    build_direction: Direction or (0,0))
+    A worker_number is an integer designating the Worker to be moved
+    as its index in the PlayerList.
+    A Direction is a tuple of two ints (x,y), each in the range
+    [-1,1], but cannot be (0,0).
+    move_direction is a Direction
+    build_direction is a Direction or (0,0) if the move is a winning move
+A PartialTurn is a tuple of (worker_number: int, move_direction: Direction)
 """
 class TurnStrategy:
     WORKER_MOVE_DISTANCE = 1
@@ -14,42 +38,25 @@ class TurnStrategy:
     """
     This function should give back the 'best' move to make with the given
     game state.
-    @buildings: a 2d list of ints from [0-4]. The outer list is a list
-                of columns, and the inner list is a list of heights
-                representing the height of each building.
-                The positive direction of the outer list represents
-                "East" and the positive direction of the inner list
-                represents "South".
-    @players: a list of two Players, where the first member of the list is
-              the player this class is representing, and the second is the
-              opposing player. A Player is a list of Workers, which are
-              tuples of two ints (x, y) representing their position on the board.
-              The x and y directions are the same as the buildings'.
-    @return: A tuple of (workernumber, direction_to_move, direction_to_build)
-             A workernumber is an integer designating the Worker to be moved
-             as its index in the given Workers list.
-             A directiontomove is a tuple of two ints (x,y), each in the range
-             [-1,1], but cannot be (0,0).
-             A directiontobuild is a tuple of two ints (x,y) each in the range
-             [-1,1]. Can be (0,0) only if the move is a winning move.
+    @buildings: A BuildingGrid that represents the board state
+    @players: A PlayerList where the first Player represents the player
+              this class is representing, and the second player represents
+              the opposing player.
+    @return: A Turn that represents the best next turn, else None if all
+             moves lead to loss.
     """
     def get_move(self, buildings, players, lookaheads):
     # List of tuples of (Turn, Turns)
     # A Turn is (WorkerNum, WorkerMoveDirection, BuildingMoveDirection)
         turn_tree = _get_node_generator(self, players, buildings) 
-    
-
 
     """
-    Gets a generator of all possible moves for the player from the given board position.
-    @players: a list of two Players, where the first member of the list is
+    Gets a generator of all possible turns for the player from the given board position.
+    @players: A PlayerList where the first member of the list is
               the player this class is representing, and the second is the
               opposing player.
-    @buildings: a list of list of heights. The outer list should be a list of columns,
-                and the inner list should be a list of cells. The positive direction
-                of the outer list should be considered "East" and the positive direction
-                of the iner list should be considerd "South".
-    @return: a generator of all possible legal moves (worker, move_direction, building_move_direction)
+    @buildings: A BuildingGrid that represents the board state 
+    @return: a generator of all possible legal Turns
     """
     @staticmethod
     def _get_node_generator(players, buildings):
@@ -66,13 +73,21 @@ class TurnStrategy:
 
         return possible_move_and_build_moves
 
+    """
+    Gets a generator of all possible moves for a given worker.
+    @worker: the Worker to determine all possible moves for
+    @players: A PlayerList where the first member of the list is
+              the player this class is representing, and the second is the
+              opposing player.
+    @buildings: A BuildingGrid that represents the board state 
+    """
     @staticmethod
     def _get_worker_moves(worker, players, buildings):
-        worker_height = TurnStrategy._get_height_at_position(buildings, worker[0], worker[1])
+        worker_height = buildings[worker[0]][worker[1]]
         all_workers = players[0] + players[1]
         for direction in TurnStrategy._gen_cardinal_directions():
             new_pos = TurnStrategy._add_tuples(worker, direction) 
-            new_height = TurnStrategy._get_height_at_position(buildings, new_pos[0], new_pos[1])
+            new_height = buildings[new_pos[0]][new_pos[1]]
             if (TurnStrategy._in_bounds(*new_pos) and
                 new_pos not in all_workers and
                 new_height - worker_height <= TurnStrategy.WORKER_HEIGHT_MOVE_DIFF):
@@ -80,6 +95,15 @@ class TurnStrategy:
             else:
                 continue
 
+    """
+    Gets a generator of all possible turns for a worker for a given move.
+    @worker: the Worker to determine all possible moves for
+    @move_direction: a Direction to move in
+    @players: A PlayerList where the first member of the list is
+              the player this class is representing, and the second is the
+              opposing player.
+    @buildings: A BuildingGrid that represents the board state 
+    """
     @staticmethod
     def _get_possible_build_moves(worker, move_direction, players, buildings):
         all_workers = [w for w in players[0] if w is not worker] + [TurnStrategy._add_tuples(worker, move_direction)] + players[1]
@@ -87,24 +111,38 @@ class TurnStrategy:
             build_pos = reduce(TurnStrategy._add_tuples, [worker, move_direction, direction], (0, 0))
             if (TurnStrategy._in_bounds(*build_pos) and
                 build_pos not in all_workers and
-                TurnStrategy._get_height_at_position(buildings, build_pos[0], build_pos[1]) < TurnStrategy.MAX_HEIGHT):
+                buildings[build_pos[0]][build_pos[1]] < TurnStrategy.MAX_HEIGHT):
                 yield (worker, move_direction, direction)
             else:
                 continue
 
+    """
+    @return: a list of all possible Directions and (0,0)
+    """
     @staticmethod
     def _gen_cardinal_directions():
-        return itertools.product(range(0 - TurnStrategy.WORKER_MOVE_DISTANCE, 1 + TurnStrategy.WORKER_MOVE_DISTANCE),
-                                 range(0 - TurnStrategy.WORKER_MOVE_DISTANCE, 1 + TurnStrategy.WORKER_MOVE_DISTANCE))
+        return itertools.product(range(0 - TurnStrategy.WORKER_MOVE_DISTANCE,
+                                        1 + TurnStrategy.WORKER_MOVE_DISTANCE),
+                                 range(0 - TurnStrategy.WORKER_MOVE_DISTANCE,
+                                        1 + TurnStrategy.WORKER_MOVE_DISTANCE))
 
-    @staticmethod
-    def _get_height_at_position(buildings, x, y):
-        return buildings[x][y]
-
+    """
+    convenience method to add two tuples of size 2 together
+    @t1: a tuple of (Number, Number)
+    @t2: a tuple of (Number, Number)
+    @return: a tuple of (Number, Number)
+    """
     @staticmethod
     def _add_tuples(t1, t2):
         return t1[0] + t2[0], t1[1] + t2[1]
 
+    """
+    convenience method to bounds check x and y values to make sure
+    they're within the board
+    @x: an Int
+    @y: an Int
+    @return: a boolean True if the given x and y are on the board, else False
+    """
     @staticmethod
     def _in_bounds(x, y):
         return x in range(0, TurnStrategy.BOARD_SIZE) and y in range(0, TurnStrategy.BOARD_SIZE)
