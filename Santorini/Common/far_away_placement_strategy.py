07ea4e5767971128567c1b14264bb9a67bed5dbb
Sreeharsha Potu <potu.s@husky.neu.edu>
2018-10-14 16:10:08 -0400

    tested far_away_placement_strategy

--- Santorini/Common/far_away_placement_strategy.py
+++ Santorini/Common/far_away_placement_strategy.py
@@ -1,7 +1,8 @@
+import math
 """
 A sub-strategy that determines the best placement to make during the start-up phase
 """
-class PlacementStrategy:
+class FarAwayPlacementStrategy:
     def __init__(self):
         pass
 
@@ -17,7 +18,43 @@ class PlacementStrategy:
              that this strategy component chooses to place the next worker.
     """
     def get_worker_placement(self, players):
-        our_workers = players[0]
-        opp_workers = players[1]
+        # get worker lists, remove workers that don't exist
+        our_workers = [worker for worker in players[0] if worker is not None]
+        opp_workers = [worker for worker in players[1] if worker is not None]
 
-       # do stuff 
+        # return (0,0) if there aren't any opponent workers
+        if (all(worker is None for worker in opp_workers)):
+            return (0,0)
+
+        max_dist_posn = (0,0)
+        max_dist = 0
+
+        # get distance between every possible posn on the board and
+        # choose the one with the greatest distance
+        for i in range(6):
+            for j in range(6):
+                # don't pick a position where a worker is already
+                if ((i, j) in our_workers or (i, j) in opp_workers):
+                    continue
+
+                dist = 0
+
+                # calculate distance between both workers
+                for worker in opp_workers:
+                    dist += self._dist_between_points(worker, (i, j))
+
+                # pick this point if it's at the maximum distance so far
+                if (dist > max_dist):
+                    max_dist_posn = (i, j)
+                    max_dist = dist
+
+        return max_dist_posn
+
+    """
+    Get the euclidian distance between two positions
+    @posn1, posn2: a tuples of 2 ints(x,y) each from [0,5] representing the
+                   positions of the points to calculate the distance of
+    @returns a number representing the euclidian distance between two points
+    """
+    def _dist_between_points(self, posn1, posn2):
+        return math.sqrt((posn1[0] - posn2[0]) ** 2 + (posn1[1] - posn2[1]) ** 2)
