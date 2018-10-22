5d7ae886aa3b9a1b7ae86369f88693b3910f8e2f
James Wu <wu.jame@husky.neu.edu>
2018-10-22 15:23:25 -0400

    fixes for various test cases

--- Santorini/Player/tree_strat.py
+++ Santorini/Player/tree_strat.py
@@ -6,7 +6,9 @@ from itertools import product
 from Santorini.Player.strategy import TurnStrategy
 from Santorini.Common.pieces import Direction
 from Santorini.Common import rulechecker
-
+import logging
+logger = logging.getLogger('tree_strat')
+logging.basicConfig(filename='tree_strat.log',level=logging.DEBUG)
 
 class TreeStrategy(TurnStrategy):
     """A strategy implementation that uses a game tree to ensure that
@@ -55,7 +57,7 @@ class TreeStrategy(TurnStrategy):
         :param Worker worker: an optional worker to move and/or build
         :param Direction move_dir: The direction to move the given
                                    worker if a worker was given
-        :param Dircetion build_dir: An optional direction the worker builds in
+        :param Direction build_dir: An optional direction the worker builds in
         :rtype bool: if we survived depth number of rounds
         """
         copied_board = copy.deepcopy(board)
@@ -63,28 +65,72 @@ class TreeStrategy(TurnStrategy):
             copied_board.move_worker(worker, move_dir)
             if build_dir:
                 copied_board.build_floor(worker, build_dir)
+            else:
+                # if there's no build, you must win this turn
+                return rulechecker.get_winner(copied_board) == pname
 
-        # if we won, we survived
-        if rulechecker.get_winner(copied_board) == pname:
+        checkwin = rulechecker.get_winner(copied_board)
+        if checkwin == pname:
+            # if we won, we survived
+            logger.info(str(pname) + " won a case Board:" + str(board) + " at Depth:" + str(depth))
+            depth -= 1
             return True
+        elif checkwin:
+            logger.info(str(pname) + " won a case Board:" + str(board) + " at Depth:" + str(depth))
+            # if we lost, we died
+            return False
 
         # base case, if there's no winner, we survived
         if depth == 0:
+            logger.info("player" + pname + "lived with this move:" + str(copied_board) + " depth:" + str(depth))
             return not rulechecker.get_winner(copied_board)
 
         # recursive case
         opp_workers = [w for w in copied_board.workers
                        if pname != w.player]
+        our_workers = [w for w in copied_board.workers
+                       if pname == w.player]
         enemy_turns = TreeStrategy.next_turn(opp_workers, copied_board)
-        for enemy_worker, enemy_move, enemy_build in enemy_turns:
-            next_board = copy.deepcopy(copied_board)
-            next_board.move_worker(enemy_worker, enemy_move)
-            if enemy_build:
-                next_board.build_floor(enemy_worker, enemy_build)
-            winner = rulechecker.get_winner(next_board)
-            if (winner and winner != pname):
-                return False
-        return TreeStrategy.do_survive(board, pname, depth - 1)
+        viable_move = False
+        if move_dir:
+            for enemy_worker, enemy_move, enemy_build in enemy_turns:
+                next_board = copy.deepcopy(copied_board)
+                next_board.move_worker(enemy_worker, enemy_move)
+                if enemy_build:
+                    next_board.build_floor(enemy_worker, enemy_build)
+                winner = rulechecker.get_winner(next_board)
+                if (winner and winner != pname):
+                    #enemy found a way to kill you on their move, return False
+                    logger.info("player " + winner + " kills with this move:" + str(next_board) + " depth:" + str(depth))
+                    return False
+
+                if depth > 1:
+                    our_turns = TreeStrategy.next_turn(our_workers, next_board)
+                    safe = False
+                    # check that we have a safe move
+                    for our_worker, our_move, our_build in our_turns:
+                        if TreeStrategy.do_survive(next_board, pname, depth - 2, worker=our_worker, move_dir=our_move, build_dir=our_build):
+                            logger.info("player " + pname + " survived with this move:" + str(next_board) + " depth:" + str(depth))
+                            continue
+                        else:
+                            safe = True
+                            break
+                    if safe:
+                        viable_move = True
+            # fell through
+            # if depth was 1, that means that we were checking if the enemy could kill us, and if so that means this should
+            # be True cause they failed to kill us
+            # if depth was 2 or more that means searching for more moves failed, and we died
+            logger.info("player " + pname +" depth:" + str(depth) + " viable_move" + str(viable_move))
+            return depth < 2 or viable_move
+        else:
+            our_turns = TreeStrategy.next_turn(our_workers, copied_board)
+            for our_worker, our_move, our_build in our_turns:
+                if TreeStrategy.do_survive(copied_board, pname, depth - 2, worker=our_worker, move_dir=our_move, build_dir=our_build):
+                    logger.info("player " + pname + " survived with this move:" + str(copied_board) + " depth:" + str(depth))
+                    return True
+            logger.info("player " + pname + " can't survive with this board:" + str(copied_board) + " depth:" + str(depth))
+            return False
 
     def get_turn(self, workers, board):
         """Return a valid turn for the list of player's worker on the board.
