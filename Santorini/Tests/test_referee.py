5955ecd471996cabfbb7e3d32e6009b2135686e8
Sreeharsha Potu <potu.s@husky.neu.edu>
2018-10-22 23:48:43 -0400

    added referee test

---
+++ Santorini/Tests/test_referee.py
@@ -0,0 +1,21 @@
+"""Unit tests for the PlacementStrat Component."""
+import unittest
+import sys
+import os
+import copy
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+from Santorini.Admin.referee import Referee
+from Santorini.Player.player import Player
+
+
+class TestReferee(unittest.TestCase):
+    """Test diagonal placement strategy"""
+
+    def setUp(self):
+        self.player1 = Player()
+        self.player2 = Player()
+
+    def test_game(self):
+        ref = Referee(self.player1, self.player2)
+
+        self.assertEqual(ref.run_game(), self.player1)
