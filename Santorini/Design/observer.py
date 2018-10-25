30c3806d45dc3dc02f3f046107593b21f867bcf3
Sreeharsha Potu <potu.s@husky.neu.edu>
2018-10-25 10:24:10 -0400

    updated observer design

--- Santorini/Design/observer.py
+++ Santorini/Design/observer.py
@@ -1,23 +1,52 @@
-"""Interface for a Observer if a game being refereeed in Santorini."""
+"""Interface for an Observer of a game being refereed in Santorini."""
 from abc import ABC, abstractmethod
 
 
-class AbstractStrategy(ABC):
-    """interface for a Referee object in Santorini."""
+class AbstractObserver(ABC):
+    """Interface for a Observer object in Santorini."""
 
-    @abstractmethod
-    def update(self, state):
-        """Receieve an event and update the view based on the given state.
-
-        :param Board state: a copy of the current game board
+    def __init__(self, referee):
+        """Create an observer object that plugs into the referee
+        
+        The method should plug into the referee that it will observe
+        in order to send and receive messages from it
+        :param referee: the referee that this 
         """
         pass
 
-    def update_game_over(self, state):
-        """Same as the update event but this notifies the observer that the
-        game is over as well.
+    @abstractmethod
+    def update_placement(self, board, placement, worker_to_player)
+        """Receives a placement and updates
+
+        A placement is a tuple of worker and
+        positon (in the form ( Worker, (row, col)),
+
+        :param Board board: a copy of the current game board
+        :param placement placement: a turn that the player inputted
+        :param worker_to_player dict: dict mapping worker to player names
+        """
+
+    @abstractmethod
+    def update_turn(self, board, turn, worker_to_player):
+        """Receives a turn and updates.
+
+        A turn is one of:
+        (None, None, None) - A no request if it couldn't find any move
+        (Worker, Direction, None) - Move request
+        (Worker, Direction, Direction). - Move+Build request
+
+        :param Board board: a copy of the current game board
+        :param turn turn: a turn that the player inputted
+        :param worker_to_player dict: dict mapping worker to player names
+        """
+        pass
+
+    @abstractmethod
+    def update_game_over(self, board):
+        """Same notifies the observer that the game is over as well.
 
 
-        :param Board state: a copy of the current game board
+        :param Board board: a copy of the current game board
+        :param player string: name of the player that won
         """
         pass
