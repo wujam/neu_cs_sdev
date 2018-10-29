bc14fa22d0dd2d0f445ab64c7ec36dc175479f18
James Wu <wujam@github.com>
2018-10-29 19:31:11 -0400

    referee impl

--- Santorini/Admin/referee.py
+++ Santorini/Admin/referee.py
@@ -3,199 +3,229 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
-from Santorini.Common.pieces import Board
+from Santorini.Common.pieces import Board, Worker
 from Santorini.Common import rulechecker
+from Santorini.Admin.player_guard import PlayerGuard
 import itertools
 import copy
+import uuid
+from enum import Enum
 
-class Referee():
+class Referee:
+
+    class PlayerResult(Enum):
+        OK = 0
+        BAD = 1
+        NEFARIOUS = 2
+    """
+    A GameResult is a tuple (PlayerResult, PlayerGuard)
+        (OK, PlayerGuard) means that the given player is the winner and won the game fairly
+        (BAD, PlayerGuard) means that the given player made an invalid move or placement
+                           and should be given a game loss
+        (NEFARIOUS, PlayerGuard) means that the given player timed out, gave malformed data,
+                                 tried to use unowned workers, or raised an exception, and
+                                 should be disconnected and disqualified
+    """
+
     """Implementation of the Referee component in Santorini."""
+    NUM_PLAYERS = 2
 
-    NUM_WORKERS = 2
-
-    def __init__(self, players):
+    def __init__(self, uuids_players, timeout):
         """Create a referee component with the associated list of players."""
-        self.players = players
-        self.board = Board()
-        self.turn = 0
+        self.players = []
+        self.timeout = timeout
+        self.uuids_to_player = {}
+        self.uuids_to_name = {}
+        for uuid, player in uuids_players:
+            player_guard = PlayerGuard(player)
+            self.uuids_to_player[uuid] = player_guard
+            self.players.append(player_guard)
 
     def run_game(self):
         """Supervise a game between players.
-
-        A referee will be able to construct a new board and have
-        access to the rules. The ref will then initialize each player
-        and then determine the "first" player in the game.
-
-        The ref will receive the player's worker placements and then
-        place them, in alternating order, on the board.
-
-        During the course of the game, the ref will call complete_turn
-        to execute turns given to him by the player.
-
-        If a game end state is reached, the ref will call determine_winner
-        to check against its list of players and the board to get the
-        winner of the game. 
-        rtype: the player that one the game
+        :rtype tuple(PlayerResult.OK, Uuid) the player won the game
+               tuple(PlayerResult.NEFARIOUS, list(Uuid)) the player did something
+                                                         untrustworthy and the
+                                                         other should win by
+                                                         default
         """
+        result = self._initialize()
+        evil_players = []
+        if result is not PlayerResult.OK:
+            evil_players.append(result[1])
+        else:
+            result, player = self._play_game(self.players)
 
-        # reset the game so previous state from another game
-        # won't affect this game
-        self._reset()
+            if result is PlayerResult.OK:
+                winner = player
+            else:
+                winner = [p_id for p_id in self.uuids_to_player if p_id is not player][0]
+            if result is PlayerResult.NEFARIOUS:
+                evil_players.append(player)
 
-        game_players = copy.copy(self.players)
-
-        # call player initialize methods at the start
-        for player in game_players:
-            player.initialize()
-
-        # place the player workers on the board
-        for worker in range(self.NUM_WORKERS):
-            for player in game_players:
-                # player made a bad placement
-                if not self.complete_placement(player):
-                    # if the game shouldn't continue finish it
-                    if not self._lose_player(player, game_players):
-                        game_players[0].game_over(True)
-                        return game_players[0]
-
-        # turn loop over all the players
-        for player in itertools.cycle(game_players):
-            # if the given turn was not valid
-            if not self.complete_turn(player):
-                # if the game shouldn't continue finish it
-                if not self._lose_player(player, game_players):
-                    game_players[0].game_over(True)
-                    return game_players[0]
-
-            # check if the game is over
-            if rulecheker.is_game_over(self.board, player.workers):
-                player.game_over(True)
-                losers = [winner for winner in game_players if winner is not player]
-                for loser in losers:
-                    loser.game_over(False)
-                    return player
+        return self._end_game(winner, evil_players)
 
     def run_n_games(self, num_games):
         """Supervises a best of num_games game series with the two players
 
         Will call run_game num_games times and tally up the score
 
-        param: num_games the number of games to play in this series
-                         this should be an odd number
-        rtype: the player that won the game series
+        :param int num_games: the number of games to play in this series
+                              this should be an odd number
+        :rtype tuple(PlayerResult.OK, Uuid) the player won the series
+               tuple(PlayerResult.NEFARIOUS, list(Uuid)) the player did something
+                                                         untrustworthy and the
+                                                         other should win by
+                                                         default
         """
 
         player_scores = [0] * len(self.players)
+        result = self._initialize()
+        evil_players = []
 
-        # a dict mapping players to the number of games they won
-        for i in range(num_games):
-            player_won = run_game()
-            players_scores[self.players.index(player)] += 1
+        if result is not PlayerResult.OK:
+            evil_players.append(result[1])
+        else:
+            # a dict mapping players to the number of games they won
+            for i in range(num_games):
+                player_order = self.players if i % 2 == 0 else reversed(self.players)
+                result, player = self._play_game(player_order)
+                if result is PlayerResult.OK:
+                    winner = player
+                else:
+                    winner = [p_id for p_id in self.uuids_to_player if p_id is not player][0]
+                if result is PlayerResult.NEFARIOUS:
+                    evil_players.append(player)
 
-        won_player = None
-        won_player_score = 0
+                result, players = self._end_game(winner, evil_players)
+                if result is PlayerResult.NEFARIOUS:
+                    for p in players:
+                        evil_players.append(p)
+                    break
 
-        for i in range(len(player_scores)):
-            if player_scores[i] > won_player_score:
-                won_player_score = player_scores[i]
-                won_player = self.players[i]
+                players_scores[self.players.index(winner)] += 1
 
-        return won_player
+        if len(evil_players) > 0:
+            return (PlayerResult.NEFARIOUS, evil_players)
+        else:
+            won_player = None
+            won_player_score = 0
 
-    def _lose_player(self, player, game_players):
-        """Kick a player out of the game
+            for i in range(len(player_scores)):
+                if player_scores[i] > won_player_score:
+                    won_player_score = player_scores[i]
+                    won_player = self.players[i]
 
-        Tell the player that they lost
+            return (PlayerResult.OK, won_player)
 
-        Check if there is more than 1 player
-        left in the game or not
-        rtype: bool
-                True if the game should continue (more than 1 player)
-                False if the game is over (only 1 player left)
+    def _initialize(self):
+        """Initialize game components and tell players what their id is
+        :rtype PlayerResult.OK or tuple(PlayerResult.NEFARIOUS, Uuid):
+            OK if initialization went fine.
+            NEFARIOUS and the nefarious player otherwise.
         """
+        self._reset_board()
+        for player_uuid, playerguard in self.uuids_to_player.items():
+            try:
+                player_guard.set_id(player_uuid)
+                name = player_guard.get_name()
+                self.uuids_to_name[player_uuid] = name
+            except PlayerException:
+                return (NEFARIOUS, player_uuid)
 
-        player.game_over(True)
-
-        # remove player from the player list
-        game_players = [winner for winner in game_players if winner is not player]
-
-        return True if len(self.players) == 1 else False
-
-    def complete_placement(self, player):
-        """Complete a placement for a given player.
-
-        This method will call the player's place_worker method in
-        conjunction with the rulecheker.
-
-        If the turn is invalid, the ref will end the game and
-        declare the opposing player as the winner.
-
-        :param player: the player who is placing a worker
-        :rtype bool
-                True if the placement was valid
-                False if there was a problem
+    def _play_game(self, players):
         """
-
-        worker, pos = player.place_worker(self.board)
-        # check if the turn isn't valid
-        if not rulechecker.can_place_worker(copy.deepcopy(self.board), worker, pos):
-            return False
-
-        self.board.place_worker(worker, pos)
-        return True
-
-    def complete_turn(self, player):
-        """Complete a turn for a given player.
-
-        This method will call the player's play_turn method in
-        conjunction with the rulechecker.
-
-        If the turn is invalid, the ref will end the game and
-        declare the opposing player as the winner.
-
-        A turn is one of:
-        (None, None, None) - A no request if it couldn't find any move
-        (Worker, Direction, None) - Move request
-        (Worker, Direction, Direction). - Move+Build request
-
-        :param player: the player who's turn it is
-        :rtype bool
-                True if the turn is valid
-                False if there was a problem
+        :param list players: List of two PlayerGuards in order of who goes first
+        :rtype GameResult: the result of the game
         """
+        # start phase
+        for player in players:
+            try:
+                player.start_game()
+            except PlayerException:
+                return (PlayerResult.NEFARIOUS, player)
+        # placement phase
+        for worker_num in range(Worker.NUM_WORKERS):
+            for player in players:
+                place_result = self._place_worker(player)
+                if place_result is PlayerResult.OK:
+                    continue
+                else:
+                    return (place_result, player)
+        # play phase
+        for player_uuid, player in itertools.cycle(self.uuids_to_player.items()):
+            turn_result = self._play_turn(player)
+            if place_result is PlayerResult.OK:
+                continue
+            else:
+                return (place_result, player)
+            if rulechecker.is_game_over(copy.deepcopy(self.board))
+                winner = self.uuids_to_player(rulechecker.get_winner(self.board))
+                return (PlayerResult.OK, winner)
 
-        # TODO process isolation for players
-        # don't need to deepcopy the board
-        # because it will be running in a fork
-        # TODO can we run just the method in a separate process
-        # what if the player wants to keep state around between turns?
+    def _place_worker(self, player):
+        """Get a placement from a player and place on the board.
+        :param PlayerGuard player: the PlayerGuard to get a placement from
+        :rtype PlayerResult: OK if worker placement went through
+                             BAD if worker placement was invalid
+                             NEFARIOUS if player did something untrustworthy
+        """
+        try:
+            worker, position = player.place_worker(copy.deepcopy(self.board))
+        except PlayerException:
+            return PlayerResult.NEFARIOUS
 
-        # get the next turn the player wants to make
-        worker, move_dir, build_dir = player.play_turn(copy.deepcopy(self.board))
+        if rulechecker.can_place_worker(self.board, worker, position):
+            self.board.place_worker(*placement)
+            return PlayerResult.OK
+        else:
+            return PlayerResult.BAD
 
-        # check case where no move is returned
-        if worker is None:
-            return False
-        
-        # check if the turn isn't valid
-        # TODO make rulechecker can_move_build return False
-        #   if the given move is not a winning move
-        #   or if a given move + build moves onto a
-        #   3 height spot
-        if not rulechecker.can_move_build(self.board, move_dir, build_dir):
-            return False
+    def _play_turn(self, player):
+        """Get a turn from a player and enact on the board.
+        :param PlayerGuard player: the PlayerGuard to get a turn from
+        :rtype PlayerResult: OK if turn went through
+                             BAD if turn was invalid
+                             NEFARIOUS if player did something untrustworthy
+        """
+        try:
+            worker, move_dir, build_dir = player.play_turn(copy.deepcopy(self.board))
+        except PlayerException:
+            return PlayerResult.NEFARIOUS
 
-        # apply move to the board since it's valid
+        if all(rulechecker.can_move_build(self.board, worker, move_dir, build_dir)):
+            self._do_move(worker, move_dir, build_dir)
+            return PlayerResult.OK
+        else:
+            return PlayerResult.BAD
+
+    def _do_move(self, worker, move_dir, build_dir=None):
+        """Perform a turn on the board
+        :param Worker worker: the worker that is moving
+        :param Direction move_dir: the worker to move in
+        :param Direction build_dir: the direction to build in
+        """
         self.board.move_worker(worker, move_dir)
+        if build_dir:
+            self.board.build_floor(worker, build_dir)
 
-        # apply build_dir to the board if there is one
-        if build_dir is not None:
-            self.board.build_floor(worker, build_direction)
+    def _end_game(self, winner, evil_players):
+        """ Notifies players of end of game.
+        :param Uuid winner: uuid of winner
+        :param list(Uuid) evil_players: the bad players
+        """
+        for player in self.uuids_to_player:
+            if player not in evil_players:
+                try:
+                    player.end_of_game(winner)
+                except PlayerException:
+                    evil_players.append(player)
 
-        return True
+        if len(evil_players) is not 0:
+            return (PlayerResult.NEFARIOUS, evil_players)
+        else:
+            return (PlayerResult.OK, winner)
 
-    def _reset(self):
-        """resets the state of the referee.
-
-        Resets the game board"""
+    def _reset_board(self)
+        """ Resets the board """
         self.board = Board()
