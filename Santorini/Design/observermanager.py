9ec696da0f850f8aa018a3088d936b38cc7c6b04
James Wu <wujam@github.com>
2018-10-31 18:19:27 -0400

    observer manager design

---
+++ Santorini/Design/observermanager.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+""" ObserverManager manages Observers """
+class ObserverManager(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def add_observer(self, observer):
+        """ Adds an observer.
+        :param Observer observer: an Observer to be added
+        """
+        pass
+
+    @abstractmethod
+    def remove_observer(self, observer):
+        """ Removes an observer.
+        :param Observer observer: an Observer to be removed
+        :rtype bool: True if successful, False if not found
+        """
+        pass
+
+    @abstractmethod
+    def remove_all_observers(self):
+        """ Removes all observers """
+        pass
+
+    @abstractmethod
+    def notify_all(self, fun_name, *args, **kwargs):
+        """ Notifies all observers by calling the given function
+        on all observers. If an observer misbehaves they will be
+        booted.
+        :param String fun_name: name of function to call
+        :param list args: arguments of function
+        :param list kwargs: keyword arguments of function
+        """
+        pass
