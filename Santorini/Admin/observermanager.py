741b9922e2815e6bf01d2a3687846be283c3b9a7
Sreeharsha Potu <potu.s@husky.neu.edu>
2018-11-03 15:22:02 -0400

    added imports to observermanager

--- Santorini/Admin/observermanager.py
+++ Santorini/Admin/observermanager.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+from Santorini.Lib.timeout import timeout
 """ ObserverManager manages Observers """
 class ObserverManager:
     def __init__(self, timeout=10):
