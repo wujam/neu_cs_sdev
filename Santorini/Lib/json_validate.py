6e5b7738366aa12548fdda4f9fb5df2eec222d00
James Wu <wujam@github.com>
2018-11-18 15:09:24 -0500

    add stub for a json validating library function

---
+++ Santorini/Lib/json_validate.py
@@ -0,0 +1,8 @@
+def validate_json(name, data):
+    """
+    Validates data with the given json schema.
+    :param str name: Name of the json schema to validate with
+    :param Json data: Json object to validate
+    :rtype bool: True if data is valid, False if data is invalid
+    """
+    pass
