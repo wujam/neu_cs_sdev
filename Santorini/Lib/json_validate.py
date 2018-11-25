340a9287872e341ae57865c3dfb57164e2e875de
Sreeharsha Potu <potu.s@husky.neu.edu>
2018-11-25 15:33:44 -0500

    fixed validate_json signature

--- Santorini/Lib/json_validate.py
+++ Santorini/Lib/json_validate.py
@@ -4,7 +4,7 @@ from jsonschema import validate
 def validate_json(schema, data):
     """
     Validates the data with the given jsonschemas path
-    :param str schema: Name of the json schema to validate with
+    :param Json schema: Name of the json schema to validate with
     :param Json data: Json object to validate
     :rtype bool: True if data matches the schema, False otherwise
     """
